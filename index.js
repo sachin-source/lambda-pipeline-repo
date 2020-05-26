39d0c324e84bb34d3b9405b7d35e3bfde20465a4
sachin-source <55872747+sachin-source@users.noreply.github.com>
2020-05-26 20:53:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var time = require('time');
+//var time = require('time');
 exports.handler = (event, context, callback) => {
-    var currentTime = new time.Date();
-    currentTime.setTimezone("America/Los_Angeles");
+    var currentTime = new Date();
+    //currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
-        body: 'The time in Los Angeles is: ' + currentTime.toString(),
+        body: 'The time in Los Angeles is: ' + currentTime.getTime(),
     });
 };
